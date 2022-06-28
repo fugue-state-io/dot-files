3fa05740c65a791d170a98e88cfe78a94fff2683
jonathon zudell <jonathon.zudell@gmail.com>
2022-06-27 19:17:37 -0500

    further cleanup

--- aws_setup.sh
+++ aws_setup.sh
@@ -1,2 +1,7 @@
 #/bin/bash
-aws-sso-util configure populate --sso-start-url https://fugue-state.awsapps.com/start/\#/ --sso-region us-east-1 --region us-east-1
+AWS_DEFAULT_SSO_START_URL=https://fugue-state.awsapps.com/start/\#/
+AWS_DEFAULT_SSO_REGION=us-east-1
+
+rm -Rf ~/.aws/sso
+
+aws-sso-util configure populate --sso-start-url $AWS_DEFAULT_SSO_START_URL --sso-region $AWS_DEFAULT_SSO_REGION --region $AWS_DEFAULT_SSO_REGION
