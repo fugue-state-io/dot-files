87373a8d52d0959cbeeff12c627da53ffbac401a
jonathon zudell <jonathon.zudell@gmail.com>
2022-06-30 20:58:49 -0500

    dotfiles boy

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,12 @@
 #/bin/bash
-export DOT_FILES=$(dirname $0)
+export BOOTSTRAP_PATH=$(realpath "$0")
+export DOT_FILES=$(dirname $BOOTSTRAP_PATH)
 #$DOT_FILES/aws_setup.sh
 #$DOT_FILES/git_id_rsa.sh
+#TODO: ADD, KUBE_PS1 stuff
+mkdir -p $DOT_FILES/.workspace/
+cp ~/.zshrc $DOT_FILES/.workspace/.zshrc
+sed -i -e '/###BEGIN_FUGUE_STATE_CONFIG###/,/###END_FUGUE_STATE_CONFIG###/d' ~/.zshrc
+echo "###BEGIN_FUGUE_STATE_CONFIG###" >> ~/.zshrc
 envsubst '$DOT_FILES' < append.rc >> ~/.zshrc
+echo "###END_FUGUE_STATE_CONFIG###" >> ~/.zshrc
